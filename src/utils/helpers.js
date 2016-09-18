a6795f80bb411d45c3cae8388c7a7768f4284b7a
sangdy <779730089@qq.com>
2016-09-18 08:52:52 +0800

    3

---
+++ src/utils/helpers.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+function searchGit (add) {
+  let address = `https://api.github.com/users/${add}`;
+  return axios.get(address)
+    .then( (res) => (
+      { getDate:res.data }
+    ))
+    .catch(function (error) {
+      alert(error);
+    });
+}
+
+export { searchGit };
+
+function getJson() {
+  let address = `https://raw.githubusercontent.com/sangdy/demodata/master/CardData.json?${Math.random()}`;
+  return axios.get(address)
+    .then( (res) => (
+      { getJson:res.data }
+    ))
+    .catch(function (error) {
+      alert(error);
+    });
+}
+
+export { getJson };
